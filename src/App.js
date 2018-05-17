b9470117f563811ca8371285c8f0412bd4ca2da9
Anna <scheul93@gmail.com>
2018-05-16 21:53:38 -0500

    putting sign in on nav bar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import NavBar from './NavBar/NavBar.js'
 import Header from './Header/Header.js'
 import Footer from './Footer/Footer.js'
 import './App.css';
-import SignIn from './SignIn/SignIn';
 
 import firebase from '@firebase/app';
 import '@firebase/auth';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Header />
-                <NavBar />
-                <div className="main"></div>
-                <Footer />
-            </div>
-        );
-    }
+	constructor(){
+		super();
+		this.state = {
+			authenticated: false
+		};
+		this.bindAuthEvent()
+	}
+
+	bindAuthEvent(){
+		firebase.auth().onAuthStateChanged(user => {
+			const userExists = user ? true : false;
+			this.setState({authenticated: userExists});
+		})
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<NavBar isAuthenticated={this.state.authenticated}/>
+				<div className="main"></div>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import SignIn from '../SignIn/SignIn';
 import './NavBar.css';
 
-const NavBar = function () {
+const NavBar = function (props) {
 	return (
 		<div className="nav-bar">
 			<a className="nav-bar__item">Submit a book</a>
 			<a className="nav-bar__item">Submissions</a>
 			<a className="nav-bar__item">Rate a book</a>
+			<SignIn isAuthenticated={props.isAuthenticated} />
 		</div>
 	);
 }
